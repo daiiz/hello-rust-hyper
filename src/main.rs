f453b4e10ba62fd4886afb9ab8913599f414d21d
daiiz <da.iz.mg@gmail.com>
2019-03-09 20:59:05 +0900

    パスを定数で持っておく

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ extern crate hyper;
 extern crate hyper_router;
 extern crate regex;
 
-#[macro_use]
-extern crate lazy_static;
-
 mod utils;
-mod endpoints;
 
 use hyper::{Body, Request, Response, Server};
 use hyper::rt::{Future};
 use hyper_router::{Route, RouterBuilder, RouterService};
 use utils::*;
-use endpoints::*;
+
+const PATH_HELLO: &str = r"/hello";
+const PATH_NUM  : &str = r"/num/(?P<a>\d+)/(?P<b>\d*)";
+const PATH_ROOT : &str = r"/";
+
 
 fn handle_hello(_: Request<Body>) -> Response<Body> {
     let body = "Hello, World!";
@@ -20,7 +20,7 @@ fn handle_hello(_: Request<Body>) -> Response<Body> {
 }
 
 fn handle_num(req: Request<Body>) -> Response<Body> {
-    let params = capture(p("num"), req.uri().path());
+    let params = capture(PATH_NUM, req.uri().path());
     let body = format!("hello: {:?}", &params);
     create_text_response(&body, "text/plain")
 }
@@ -32,9 +32,9 @@ fn handle_root(_: Request<Body>) -> Response<Body> {
 // Result<T,E>: failableな処理の結果を表現する列挙型
 fn routes() -> Result<RouterService, std::io::Error> {
     let router = RouterBuilder::new()
-        .add(Route::get(p("hello")).using(handle_hello))
-        .add(Route::get(p("num")).using(handle_num))
-        .add(Route::get(p("root")).using(handle_root))
+        .add(Route::get(PATH_HELLO).using(handle_hello))
+        .add(Route::get(PATH_NUM).using(handle_num))
+        .add(Route::get(PATH_ROOT).using(handle_root))
         .build();
     // Promiseのresolveみたいなもの？
     Ok(RouterService::new(router))
